b17d84c9a2d97ef13902bc67f172d2310d326395
Chad Fernandez <contact@chadfernandez.me>
2024-01-06 16:52:44 +0800

    feat: added universeTypes or invTypes seeding to database

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,14 @@
 import { createTRPCRouter } from "~/server/api/trpc";
+import { seedRouter } from "./routers/seed";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  seed: seedRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
